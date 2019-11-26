479c2146401f0b0c9cbe64e79baa0773bbbf367f
ken <ken.huang@rccchina.com>
2019-11-26 13:08:32 +0800

    update

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,30 @@
 #!/usr/bin/env node	
 
-console.log('hello')
-
 const version = require('../package').version
 const program = require('commander')
 
 program
   .version(version, '-v, --version')
+  .usage('[options]')
+  .option('-n, --no-sort', 'not need sort')
+  .option('-i, --indent <num>', 'indent for json', 2)
 
 program
-  .command('create')
-  .action(console.log)
+  .arguments('<command>')
+  .action((cmd) => {
+    // console.log(cmd)
+    program.outputHelp()
+  })
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+}
 
 program.parse(process.argv)
+
+// console.log(program.indent)
+// console.log(program.sort)
+
+if (program.sort === false) {
+
+}
