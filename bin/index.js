bf27918064802ca639c18f98d64ba54be67de666
ken <ken.huang@rccchina.com>
2019-11-29 18:16:34 +0800

    update

--- bin/index.js
+++ bin/index.js
@@ -11,24 +11,18 @@ program
   .version(version, '-v, --version')
   .name(CLI_NAME)
   .usage('[options] <json-file> ' + chalk.yellow('OR') + ` <output-json> | ${CLI_NAME}`)
-  .option('-n, --no-sort', 'not need sort keys')
   .option('-r, --replace', 'replace the file directly, only <json-file> mode')
   .option('-i, --indent <num>', 'indent for json', 2)
-  .action((cmd, obj = []) => {
-    if (!obj.length && process.stdin.isTTY) {
+  .option('--no-sort', 'not need sort keys')
+  .action((cmd, args = []) => {
+    if (!args.length && process.stdin.isTTY) {
       console.log(chalk.cyan('Try format your JSON!\n'))
       program.outputHelp()
       process.exit(1)
     }
 
-    if (obj.length > 1) {
-      console.log(chalk.yellow('ERROR: More than one file supplied!\n'))
-      program.outputHelp()
-      process.exit(1)
-    }
-
     process.stdin.isTTY 
-      ? formatFileJSON(obj[0], cmd)
+      ? formatFileJSON(args, cmd)
       : formatStdinJSON(cmd)
   })
   
